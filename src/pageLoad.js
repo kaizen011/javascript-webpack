09a00ee0645fc923133234412f28446db193ee70
Kaizen <125158784+kaizen011@users.noreply.github.com>
2023-08-23 06:50:01 -0500

    Added tab file, clickable li, (fixed/sticky positioning)

--- src/pageLoad.js
+++ src/pageLoad.js
@@ -20,23 +20,57 @@ function createAndAppendElements(){
 
     contentDiv.appendChild(midbar);
 
+    //navbar
+
+    const navbar = document.createElement('nav');
+    navbar.classList.add('navbar')
+    const ul = document.createElement('ul');
+    ul.classList.add('nav-list');
+    const itemi = document.createElement('li');
+    const itemi1 = document.createElement('li');
+    const itemi2 = document.createElement('li');
+    itemi.textContent = 'Tab';
+    itemi1.textContent = 'Tab1';
+    itemi2.textContent = 'Tab2';
+
+    itemi1.classList.add('midtab')
+
+    //added click function to clear page
+    itemi.addEventListener('click', function(){
+        contentDiv.innerHTML = '';
+        contentDiv.appendChild();
+    })
+
+    ul.appendChild(itemi);
+    ul.appendChild(itemi1);
+    ul.appendChild(itemi2);
+
+    
+    navbar.appendChild(ul);
+
+    contentDiv.appendChild(navbar);
 
     // Content Code
     const services = document.createElement('div');
     services.classList.add('services');
-    contentDiv.appendChild(services);
+    
 
      const service1 = document.createElement('div');
      const service2 = document.createElement('div');
      const service3 = document.createElement('div');
      const service4 = document.createElement('div');
 
+   
 
-     service1.textContent = 'Hello'
-     service2.textContent = 'Hello'
-     service3.textContent = 'Hello'
-     service4.textContent = 'Hello'
+     let img = document.createElement("img")
+     img.setAttribute("src", "https://www.e-spincorp.com/wp-content/uploads/2018/02/deeplearning-security.png");
+     img.setAttribute("height", "280px");
+     img.setAttribute("width", "250px");
 
+     service1.appendChild(img); 
+     service2.appendChild(img.cloneNode(true));
+     service3.appendChild(img.cloneNode(true));
+     service4.appendChild(img.cloneNode(true));
     
 
      services.appendChild(service1);
@@ -45,7 +79,7 @@ function createAndAppendElements(){
      services.appendChild(service4);
 
 
-
+     contentDiv.appendChild(services);
 }
 
 
